1698a481c7d007a8748b2180b53839e175acf685
debanshu <debanshupanigrahi536052gmail.com>
2026-03-08 22:35:23 +0530

    modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,24 @@ import * as vscode from "vscode";
 import { App } from "./app/App";
 import { checkVersion } from "./utils/helper";
 export async function activate(context: vscode.ExtensionContext) {
-  await checkVersion(context);
-  const app: App = new App();
-  context.subscriptions.push(
-    vscode.commands.registerCommand("quickserve.run", async () => {
-      await app.start();
-    })
-  );
-  context.subscriptions.push(
-    vscode.commands.registerCommand("quickserve.kill", async () => {
-      await app.stop();
-    })
-  );
-  context.subscriptions.push(app);
+  console.info("activating...");
+  try {
+    await checkVersion(context);
+    const app: App = new App();
+    context.subscriptions.push(
+      vscode.commands.registerCommand("quickserve.run", async () => {
+        await app.start();
+      }),
+    );
+    context.subscriptions.push(
+      vscode.commands.registerCommand("quickserve.kill", async () => {
+        await app.stop();
+      }),
+    );
+    context.subscriptions.push(app);
+  } catch (error) {
+    console.error("Activation error:", error);
+    throw error;
+  }
 }
 export function deactivate() {}
