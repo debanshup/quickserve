92ab239da2b8e07ddc6b8da8c9661c40084f2690
debanshup <debanshupanigrahi53605@gmail.com>
2026-03-23 23:24:29 +0530

    modified:   src/StatusBarUI.ts

--- src/StatusBarUI.ts
+++ src/StatusBarUI.ts
@@ -2,13 +2,11 @@ import { StatusBarAlignment, StatusBarItem, window } from "vscode";
 import { Config } from "./utils/config";
 import { ThemeColor } from "vscode";
 const { getShowStatusBar } = Config;
-export class StatusbarUI {
-   static _statusbar: StatusBarItem;
-  // public static isRunning: boolean = false;
-  // public static port: number;
-  private static get statusBar() {
-    if (!StatusbarUI._statusbar) {
-      StatusbarUI._statusbar = window.createStatusBarItem(
+class StatusbarUI {
+  private _statusbar: StatusBarItem | undefined;
+  private get statusBar() {
+    if (!this._statusbar) {
+      this._statusbar = window.createStatusBarItem(
         StatusBarAlignment.Right,
         100,
       );
@@ -16,37 +14,20 @@ export class StatusbarUI {
       if (getShowStatusBar()) {
         // console.log("status bar set to show");
 
-        StatusbarUI._statusbar.show();
+        this._statusbar.show();
       }
     }
-    return StatusbarUI._statusbar;
+    return this._statusbar;
   }
-
-  /**
-   * Initializes the status bar UI by setting its default state.
-   */
-  static init() {
-    StatusbarUI.run();
-  }
-
-  // /**
-  //  * Updates the status bar to indicate a working state with an optional message.
-  //  *
-  //  * @param msg - The message to display (default: "Working...").
-  //  */
-
-  // static showStatusText(msg: string = "Working...") {
-  //   StatusbarUI.statusBar.text = `$(pulse) ${msg}`;
-  // }
 
   /**
    * Sets the status bar to the "Serve" state, with appropriate tooltip and command.
    */
-  public static run() {
-    StatusbarUI.statusBar.color = undefined;
-    StatusbarUI.statusBar.text = `$(play-circle) Start QuickServe`;
-    StatusbarUI.statusBar.tooltip = "Launch local development server";
-    StatusbarUI.statusBar.command = "quickserve.run";
+  public showStart() {
+    this.statusBar.color = undefined;
+    this.statusBar.text = `$(play-circle) Start QuickServe`;
+    this.statusBar.tooltip = "Launch local development server";
+    this.statusBar.command = "quickserve.run";
   }
 
   /**
@@ -54,17 +35,20 @@ export class StatusbarUI {
    *
    * @param port - The port number to display in the status bar.
    */
-  public static kill(port: number) {
-    StatusbarUI.statusBar.text = `$(stop-circle) QuickServe: ${port}`;
-    StatusbarUI.statusBar.tooltip = `Stop serving on ${port}`;
-    StatusbarUI.statusBar.command = "quickserve.kill";
-    StatusbarUI.statusBar.color = new ThemeColor('statusBarItem.warningForeground');
+  public showStop(port: number) {
+    this.statusBar.text = `$(stop-circle) QuickServe: ${port}`;
+    this.statusBar.tooltip = `Stop serving on ${port}`;
+    this.statusBar.command = "quickserve.kill";
+    this.statusBar.color = new ThemeColor("statusBarItem.warningForeground");
   }
 
   /**
    * Disposes of the status bar item, freeing associated resources.
    */
-  public static dispose() {
-    StatusbarUI.statusBar.dispose();
+  public dispose() {
+    this.statusBar.dispose();
   }
 }
+
+export const statusbarUi = new StatusbarUI();
+statusbarUi.showStart();
