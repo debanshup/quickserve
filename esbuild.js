0e479d4e76d2829536aedda85baff9af0b784ed0
debanshup <debanshupanigrahi53605@gmail.com>
2026-03-23 23:22:56 +0530

    modified:   esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -35,8 +35,8 @@ async function main() {
 		sourcesContent: false,
 		platform: 'node',
 		outfile: 'dist/extension.js',
-		external: ['vscode', 'selfsigned', 'chokidar'],
-		logLevel: 'debug',
+		external: ['vscode'],
+		logLevel: 'silent',
 		plugins: [
 			/* add to the end of plugins array */
 			esbuildProblemMatcherPlugin,
